58bd4b187ddcb6a14acda8303b46dbacd3ee6400
qasim29 <qsimmohammad534@gmail.com>
2020-05-21 07:35:50 +0500

    made a new project and created important files

---
+++ Dataship/ActivationPanel.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Dataship
+{
+    /// <summary>
+    /// Interaction logic for ActivationPanel.xaml
+    /// </summary>
+    public partial class ActivationPanel : Window
+    {
+        public ActivationPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
