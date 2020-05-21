8efd3278946d013e734756947a12425468302754
qasim29 <qsimmohammad534@gmail.com>
2020-05-22 02:59:14 +0500

    created new forms

--- docata/Program.cs
+++ docata/Program.cs
@@ -16,7 +16,7 @@ namespace docata
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new mainwin());
         }
     }
 }
--- docata/mainwin.cs
+++ docata/mainwin.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace docata
 {
-    public partial class Form1 : Form
+    public partial class mainwin : Form
     {
-        public Form1()
+        public mainwin()
         {
             InitializeComponent();
         }
